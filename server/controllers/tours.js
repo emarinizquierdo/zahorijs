7f28e4592297cbb2ae31dffb185ccfa27bd7be21
Edu <e.marin.izquierdo@gmail.com>
2016-07-15 20:47:16 +0200

    adding tour

--- server/controllers/tours.js
+++ server/controllers/tours.js
@@ -1,6 +1,7 @@
 //File: controllers/steps.js
 var mongoose = require('mongoose');
 var Tours = mongoose.model('Tours');
+var Users = mongoose.model('Users');
 
 //GET - Return all Steps in the DB
 exports.findAllTours = function(req, res) {
@@ -19,7 +20,12 @@ exports.findAllTours = function(req, res) {
 //GET - Return a Tours with specified ID
 exports.findById = function(req, res) {
 
-    Tours.findOne({ apiKey: req.params.apikey, id: req.params.id }, function(err, tour) {
+
+
+    Tours.findOne({
+        apiKey: req.params.apikey,
+        id: req.params.id
+    }, function(err, tour) {
         if (err) return res.send(500, err.message);
 
         console.log('GET /tours/' + req.params.apikey);
@@ -35,36 +41,78 @@ exports.addTours = function(req, res) {
     console.log('POST');
     console.log(req.body);
 
-    if (req.body._id) {
+    if (req && req.user) {
 
-        Tours.findById(req.body._id, function(err, tour) {
 
-            tour.apiKey = req.body.apiKey;
-            tour.id = req.body.id;
-            tour.name = req.body.name;
-            tour.steps = req.body.steps;
-            tour.published = req.body.published;
+        Users.findOne({
+            email: req.user.email
+        }, function(err, user) {
 
-            tour.save(function(err, tour) {
-                if (err) return res.status(500).send(err.message);
-                res.status(200).jsonp(tour);
-            });
+            if (err) return res.status(500).send(err.message);
+            console.log("el user es: " + user);
+            console.log('el api key es: ' + user.apiKey + " y el otro es " + req.body.apiKey);
+
+            if (user.apiKey == req.body.apiKey) {
+
+                for (var i = 0; i < user.apps.length; i++) {
+                    console.log('el appid es: ' + user.apps[i].appId + " y el otro es " + req.body.id);
+                    if (user.apps[i].appId == req.body.id) {
+                        console.log("aqui entra");
+                        if (req.body._id) {
+                            Tours.findById(req.body._id, function(err, tour) {
+
+                                tour.apiKey = req.body.apiKey;
+                                tour.id = req.body.id;
+                                tour.name = req.body.name;
+                                tour.steps = req.body.steps;
+                                tour.published = req.body.published;
+
+                                tour.save(function(err, tour) {
+                                    if (err) return res.status(500).send(err.message);
+                                    return res.status(200).jsonp(tour);
+                                });
+                            });
+
+                        } else {
+                            console.log("aqui tambien entra");
+                            var tour = new Tours({
+                                apiKey: req.body.apiKey,
+                                id: req.body.id,
+                                name: req.body.name,
+                                steps: req.body.steps,
+                                published: req.body.published
+                            });
+
+                            tour.save(function(err, tour) {
+                                if (err) return res.status(500).send(err.message);
+                                return res.status(200).jsonp(tour);
+                            });
+                        }
+                    }
+                }
+                            res.status(404).send('not found');
+            }
+
         });
 
     } else {
-
-        var tour = new Tours({
-            apiKey: req.body.apiKey,
-            id: req.body.id,
-            name: req.body.name,
-            steps: req.body.steps,
-            published: req.body.published
-        });
-
-        tour.save(function(err, tour) {
-            if (err) return res.status(500).send(err.message);
-            res.status(200).jsonp(tour);
-        });
+        res.status(403).send('forbiden');
     }
 
 };
+
+
+function checkTourPermission() {
+
+    Users.find({
+        email: req.user.email
+    }, function(err, user) {
+
+        if (err) return res.status(500).send(err.message);
+
+        user.apiKey;
+
+
+    });
+
+}
