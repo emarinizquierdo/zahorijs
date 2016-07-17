77d72b2bd0d887903f8aa4aecda0e5e19c007e1f
Edu <e.marin.izquierdo@gmail.com>
2016-07-17 18:06:08 +0200

    server fixed

--- server/app.js
+++ server/app.js
@@ -16,18 +16,19 @@ var express = require("express"),
 
 /* Express configuration */
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
-var port      = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var mongodbHost = process.env.OPENSHIFT_MONGODB_DB_HOST || 'localhost';
 var mongodbPort = process.env.OPENSHIFT_MONGODB_DB_PORT || 27017;
 var mongoUrl = 'mongodb://' + mongodbHost + ':' + mongodbPort + '/zahorijs';
-
+var mongo_url = process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/zahorijs';
 
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/../dist/index.html'));
 });
+
 app.use(express.static(path.join(__dirname + '/../dist')));
-//app.use(express.static(__dirname + '/../dist'));
+
 app.use(bodyParser.urlencoded({
     extended: false
 }));
@@ -45,12 +46,12 @@ app.use(routes);
 
 
 /* MongoDB connect */
-mongoose.connect(mongoUrl, function(err, res) {
+mongoose.connect(mongo_url, function(err, res) {
     if (err) {
         console.log('ERROR: connecting to Database. ' + err);
     }
-    app.listen(port, ipaddress, function() {
-        console.log("Node server running on " + ipaddress + ":" + port);
+    app.listen(server_port, server_ip_address, function() {
+        console.log("Listening on " + server_ip_address + ", server_port " + server_port)
     });
 });
 
