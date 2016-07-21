cac9f902844d6cdc9302c97ed03808b47c5df7ea
Edu <e.marin.izquierdo@gmail.com>
2016-07-21 17:23:22 +0200

    updated

--- admin/properties/index.js
+++ admin/properties/index.js
@@ -11,17 +11,9 @@
                 icons: _connector + "/icons/bubble",
                 tour: "http://localhost:8080" + _connector + "/tours",
             },
-            dev: {
-                icons: _connector + "/icons/bubble",
-                tour: _connector + "/tours",
-            },
-            au: {
-                icons: _connector + "/icons/bubble",
-                tour: _connector + "/tours",
-            },
             pro: {
                 icons: _connector + "/icons/bubble",
-                tour: "http://localhost:8080" + _connector + "/tours",
+                tour: "https://zahorijs-nefele.rhcloud.com" + _connector + "/tours",
             }
 
         }
