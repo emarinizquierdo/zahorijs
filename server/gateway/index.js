d74e52bab235f4656579ad9b7dd3ecaf98719290
Edu <e.marin.izquierdo@gmail.com>
2016-10-14 18:16:36 +0200

    updated

--- server/gateway/index.js
+++ server/gateway/index.js
@@ -1,11 +1,19 @@
 var braintree = require("braintree");
-var config = require('../config.json');
+var config;
+
+var _env = process.env.NODE_ENV || 'development';
+
+if (_env == "production") {
+    config = require('../config');
+} else {
+    config = require('../config.json');
+}
 
 var gateway = braintree.connect({
     environment:  braintree.Environment.Sandbox,
-    merchantId:   config['MERCHANT_SANDBOX_ID'],
-    publicKey:    config['PUBLIC_SANDBOX_KEY'],
-    privateKey:   config['PRIVATE_SANDBOX_KEY']
+    merchantId:   config[_env].MERCHANT_SANDBOX_ID,
+    publicKey:    config[_env].PUBLIC_SANDBOX_KEY,
+    privateKey:   config[_env].PRIVATE_SANDBOX_KEY
 });
 
 module.exports = gateway;
